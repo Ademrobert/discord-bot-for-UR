338d784072c58cf8f5d849ed13acf205c48305b0
timppaB <adembenothman@gmail.com>
2020-06-08 18:12:31 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -152,8 +152,38 @@ client.login(config.bot.token)
         .setColor(15844367)
         .setDescription("Website Is here clikc on this link --> http://log-in-system.herokuapp.com/");
       message.channel.send(embed);
+    }else{
+      function ent(id, message) {
+        opts = {
+            method: 'GET',
+            headers: {
+                "authorization": 'Bot ' + config.bot.token,
+                "Content-Type": 'application/json'
+            },
+        }
+    
+        fetch('https://discordapp.com/api/v6/applications/' + '598419143661846528' + '/entitlements?user_id=' + id + '&with_payments=true', opts).then(res => res.json()).then(b => { //684393832086765632
+            console.log(b.length)
+            if(b.length==0) {
+                message.channel.send('None!')
+            } else {
+                b.forEach(e=>{
+                    if(e.toString().length>5) {
+                        e.sku_id=e.sku_id.replace('598419143661846528', 'Move & Kill')
+                        client.fetchUser(e.user_id).then(u=>{
+                            e.user_id=u.username
+                            message.author.send(JSON.stringify(e))
+                        })
+                    }
+                })
+                message.channel.send('Done!')            
+            }
+        })  
+    }
     }
   });
 
   let xpAdd = Math.floor(Math.random() * 7) + 8;
   console.log(xp);
+
+  
