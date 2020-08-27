0a777fb483ceac821cecb009be3f2c5ef0bbb468
timppaB <adembenothman@gmail.com>
2020-08-27 20:35:20 +0300

    fixing member counter

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 module.exports = (client, member) => {
 
-    const channel = member.guild.channels.find(ch => ch.name === 'welcome');
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
     if (!channel) return;
     channel.send(`Welcome ${member.user} to this server. Don't forget to read the rules and to have fun!`).catch(console.error);
-    member.addRole(member.guild.roles.find(role => role.name === "Member"));
+    member.addRole(member.guild.roles.cache.find(role => role.name === "Member"));
   };
