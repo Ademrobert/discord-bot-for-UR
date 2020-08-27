e9f4283e4c527e79621b131965a73da9241cd348
timppaB <adembenothman@gmail.com>
2020-08-27 23:48:12 +0300

    fixing music command

--- src/music/index.js
+++ src/music/index.js
@@ -122,7 +122,7 @@ function skip(client, message, serverQueue) {
     );
   if (!serverQueue)
     return message.channel.send("There is no song that I could skip!");
-  serverQueue.connection.dispatcher.end();
+  serverQueue.connection.dispatcher.destroy();
 }
 
 function stop(client, message, serverQueue) {
@@ -132,7 +132,7 @@ function stop(client, message, serverQueue) {
       "You have to be in a voice channel to stop the music!"
     );
   serverQueue.songs = [];
-  serverQueue.connection.dispatcher..destroy()();
+  serverQueue.connection.dispatcher.destroy();
 }
 
 function play(guild, song) {
