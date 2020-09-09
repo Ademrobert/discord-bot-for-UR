1d799d6a4f2ad5ebd5f18b9473feb8e2f228e52e
timppaB <adembenothman@gmail.com>
2020-09-09 21:42:21 +0300

    sim

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -3,5 +3,5 @@ module.exports = (client, member) => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
     if (!channel) return;
     channel.send(`Welcome ${member.user} to this server. Don't forget to read the rules and to have fun!`).catch(console.error);
-    member.addRole(member.guild.roles.cache.find(role => role.name === "Member"));
+   // member.addRole(member.guild.roles.cache.find(role => role.name === "Member"));
   };
