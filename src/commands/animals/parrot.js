d7e6cb6189dfbf984a2a96da56301313c0e40841
timppaB <adembenothman@gmail.com>
2020-10-25 23:01:37 +0200

    fixed parrot command

--- src/commands/animals/parrot.js
+++ src/commands/animals/parrot.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
-const { RichEmbed, Attachment } = require('discord.js');
 
 module.exports.run = async(bot, message, args) => {
   console.log(`${message.author.tag} used the yeet parrot command.`)
   randomPuppy('parrots')
             .then(url => {
-                const embed = new RichEmbed()
+                const embed = new Discord.MessageEmbed()
                 
                 .setTitle(`Parrot`)
                 .setFooter(`Requested by ${message.author.tag}`)
