711abf8b2ed960d18872a6e5755b565cf31c3cc1
timppaB <adembenothman@gmail.com>
2020-10-25 23:05:23 +0200

    fixed panda command

--- src/commands/animals/panda.js
+++ src/commands/animals/panda.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
-const { RichEmbed, Attachment } = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     console.log(`${message.author.tag} used the Panda command.`)
     randomPuppy('pandas')
         .then(url => {
-            const embed = new RichEmbed()
+            const embed = new Discord.MessageEmbed()
 
                 .setTitle(`Panda`)
                 .setFooter(`Requested by ${message.author.tag}`)
