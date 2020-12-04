6f1784cbd5378e02e61135b121c9890e2bfb09d4
Ademrobert <adembenothman@gmail.com>
2020-12-04 20:50:17 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -271,7 +271,7 @@ client.on("message", async message => {
         const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
         if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
 
-        const leaderboard = Levels.computeLeaderboard(client, rawLeaderboard);
+        const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard);
 
         console.log(leaderboard)
         const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
