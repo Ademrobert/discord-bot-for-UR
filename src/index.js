635e241b7a14ed75b35f53b5afee4ab17d49a023
Ademrobert <adembenothman@gmail.com>
2020-12-04 16:03:58 +0200

    created a level system

--- src/index.js
+++ src/index.js
@@ -270,7 +270,7 @@ client.on("message", async message => {
         const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
         if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
 
-        const leaderboard = Levels.computeLeaderboard(bot, rawLeaderboard);
+        const leaderboard = Levels.computeLeaderboard(client, rawLeaderboard);
 
         const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
 
