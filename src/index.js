80ced1366b90d296b4b72f0a9036006ebbcfa354
Ademrobert <adembenothman@gmail.com>
2020-12-04 23:07:36 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-Discord.constants.DefualtOptions.ws.properties.$browser = "Discord Android";
 const { RichEmbed, Attachment } = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config");
