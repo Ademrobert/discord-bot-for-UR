e7206a78bdddf110b3de4b2460196351210a8851
Ademrobert <adembenothman@gmail.com>
2020-12-04 23:32:38 +0200

    fixed Author command

--- src/commands/info/author.js
+++ src/commands/info/author.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args) => {
         .setThumbnail((client.displayAvatarURL))
         .setColor("#000000")
         .addField("Twitter", `[here](don't have)`, inline)
-        .addField("Discord", `Adem#4636`, true)
+        .addField("Discord", `Adem#4636`, `</> Giuliopime` true)
         .addField("Discord Server", `[here](don't have)`, inline, true)
         .addField("E-Mail", `Adembenothman@gmail.com`, inline, true)
         .addField("Website", `[here](i don't have jet)`, inline, true)
