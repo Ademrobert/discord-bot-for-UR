f377b297a9d00a87f4dbcc07f299dcd70a70fa0a
Ademrobert <adembenothman@gmail.com>
2020-12-04 15:34:36 +0200

    created a level system

--- src/index.js
+++ src/index.js
@@ -232,6 +232,54 @@ client.on('message', (message) => {
 
 });
 
+// Leveling system
+
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+const Levels = require('discord-xp')
+
+Levels.setURL("MONGODB_URL")
+
+
+client.on("ready", bot => {
+    console.log('Bot is Online!')
+})
+
+bot.on("message", async message => {
+    if (!message.guild) return;
+    if (message.author.bot) return;
+
+    //const prefix = '?';
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    const randomXp = Math.floor(math.random() * 9) + 1; //Random amont of XP until the number you want + 1
+    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
+    if (hasLeveledUp) {
+        const user = await Levels.fetch(message.author.id, message.guild.id);
+        message.channel.send(`You leveled up to ${user.level}! Keep it going!`);
+    }
+
+    //Rank
+    if(command === "rank") {
+        const user = await Levels.fetch(message.author.id, message.guild.id);
+        message.channel.send(`You are currently level **${user.level}**!`)
+    }
+
+    //Leaderboard
+    if(command === "leaderboard" || command === "lb") {
+        const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
+        if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
+
+        const leaderboard = Levels.computeLeaderboard(bot, rawLeaderboard);
+
+        const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
+
+        message.channel.send(`${lb.join("\n\n")}}`)
+    }
+})
+
 
 client.on("message", async message => {
 
