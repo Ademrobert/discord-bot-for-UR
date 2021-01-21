a7ab85b224707b626da7d48e117f7ee56bbc2854
Adem <50057725+Ademrobert@users.noreply.github.com>
2021-01-21 11:50:10 +0200

    fixed Snipe Command
    Dylan placed Bot instead of Client.snipes

--- src/commands/memes/snipe.js
+++ src/commands/memes/snipe.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js')
 
-module.exports.run = async (bot, message, args) => {
-    const msg = bot.snipes.get(message.channel.id)
+module.exports.run = async (client, message, args) => {
+    const msg = client.snipes.get(message.channel.id)
     const embed = new Discord.MessageEmbed()
     .setAuthor(msg.author, msg.member.user.displayAvatarURL())
     .setDescription(msg.content)
-    .setFooter('Get Sniped lol')
+    .setFooter('Get Sniped!')
     .setTimestamp();
     message.channel.send(embed);
 }
